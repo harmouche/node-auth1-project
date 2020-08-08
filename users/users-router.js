228e66747d915552655689d1c80e7b1bce77a52f
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-08-07 22:22:41 -0400

    done

--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-const Users = require("./users-model");
-// const restricted = require('../auth/restricted-middleware');
+const Users = require("./users-model.js");
+const restricted = require('../auth/restricted-middleware');
 
-router.get("/", /*restricted, */(req, res) => {
+router.get("/", restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
